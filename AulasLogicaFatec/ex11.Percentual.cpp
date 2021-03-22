08e8da4ac6ce594c21e80790f21a1da1dcb36028
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2021-03-22 18:53:21 -0300

    Create ex11.Percentual.cpp

---
+++ AulasLogicaFatec/ex11.Percentual.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+main(){
+	int valor, imposto;
+	float ano;
+	
+	printf("Digite o ano do carro: ");
+	scanf("%i", &ano);
+	
+	printf("Digite o valor do carro R$ ");
+	scanf("%f", &valor);
+	
+	if(ano >= 1990)
+	{
+		imposto = valor*(1.5/100);
+		printf("Valor do imposto R$ %f a 1.5%.", imposto);
+	}
+	else
+	{
+		imposto = valor*(1/100);
+		printf("Valor do imposto R$ %f a 1%.", imposto);
+	}
+}
+
+
