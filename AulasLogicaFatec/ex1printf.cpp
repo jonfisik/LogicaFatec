fc2e756d5ecf9e5286612d19d6ef138d221fbf1a
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2021-03-22 18:50:11 -0300

    Create ex1printf.cpp

---
+++ AulasLogicaFatec/ex1printf.cpp
@@ -0,0 +1,9 @@
+// Aula de lógica de programação - fatec itaquera - Jonatan Paschoal 08-03-2021
+
+#include<stdio.h> //carrega a biblioteca
+
+main()
+{
+	printf("Ola seja bem-vindo."); // imprime uma mensagem
+	printf("\n\nAula de C - logica de programacao fatec itaquera.");
+}
