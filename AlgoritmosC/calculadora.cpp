de84e93c2d526fb8e0a9412daa27eebbee920ac8
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2021-04-07 17:20:19 -0300

    Update calculadora.cpp

--- AlgoritmosC/calculadora.cpp
+++ AlgoritmosC/calculadora.cpp
@@ -2,25 +2,26 @@
 #include<stdlib.h> // biblioteca padrão que controla processos e alocação de memoria etc.
 
 main(){
-	printf("===========================\n");
-    printf("======Calculadora1.0 ======\n");
-    printf("===========================\n");
+	printf("=============================\n");
+    printf("=======Calculadora1.0 =======\n");
+    printf("=============================\n");
 // variáveis
     float num1, num2, soma, sub, mult, div;
-// comandos
+// comandos para enviar msn para a tela e receber os dados
     printf("Digite dois numeros: \n");
     scanf("%f %f", &num1, &num2);
+// operações com as variáveis
     soma = num1+ num2;
     sub = num1 - num2;
     mult = num1*num2;
     div = num1/num2;
-
+// imprimir os resultados
 	printf("\n");
     printf("Soma %f + %f = %f \n", num1, num2, soma);
     printf("Subutracao %f - %f = %f \n", num1, num2, sub);
     printf("Produto %f x %f = %f \n", num1, num2, mult);
     printf("Divisao %f / %f = %f \n", num1, num2, div);
 
-    printf("===========================\n");
-    printf("===========================\n");
+    printf("=============================\n");
+    printf("=============================\n");
 }
