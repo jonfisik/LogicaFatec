9c2578c7f2615c5154f7e96cb308f96e0c3c0a3e
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2021-04-07 16:19:45 -0300

    Create conceitosImportantes.cpp

---
+++ AlgoritmosC/conceitosImportantes.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/* Jonatan Paschoal 06/04/2021
+Bloco de comentários - As linhas dentro deste bloco não serão executados
+Instrução, função - linhas que serão executadas. As instruções estão separadas por ponto e vírgula
+Endentação - boa prática para manter o código organizado.*/
+
+// Instrução
+main(){
+	//Endentação
+	printf("Programando em C!!!");
+}
