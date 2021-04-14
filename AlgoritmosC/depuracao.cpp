bef49f8cb8f5884a3dc045ebb7bbb77d1c2ed212
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2021-04-13 23:57:19 -0300

    Create depuracao.cpp

---
+++ AlgoritmosC/depuracao.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+Jontan P Paschoal
+Depuração -  testar e identificar erros. Seleciona a linha, 
+criando um break point e depurar de acordo com as id'es.
+*/
+
+int main()
+{
+    printf("Teste linha 1 \n");
+    printf("Teste linha 2 \n");
+    printf("Teste linha 3 \n");
+    printf("FIM \n");
+    return 0;
+}
