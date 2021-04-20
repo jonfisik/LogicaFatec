40a24ea4210b4ca66d7a1c4a0a280c62f88d8948
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2021-04-20 20:31:40 -0300

    Create constante.cpp

---
+++ AlgoritmosC/constante.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define a 100
+
+int main()
+{
+    //cont tipo constante = valor;
+    //const int a = 5;
+    printf("\t===Constante===\n");
+    printf("\t\t%i\n", a);
+    printf("\t===Constante===");
+    return 0;
+}
