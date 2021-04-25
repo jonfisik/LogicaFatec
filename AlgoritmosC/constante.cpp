acb748c1ab4748f85f943c11f82798df7a0998fb
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2021-04-25 19:21:29 -0300

    Update constante.cpp

--- AlgoritmosC/constante.cpp
+++ AlgoritmosC/constante.cpp
@@ -1,14 +1,18 @@
+/*
+Autor: Jonatan P. Paschoal
+email: jonfisik@hotmail.com
+status: Professor - física
+data: 25/04/2021
+*/
 #include <stdio.h>
 #include <stdlib.h>
-
+// comando nome_da_constante valor
 #define a 100
 
-int main()
-{
+main(){
     //cont tipo constante = valor;
     //const int a = 5;
     printf("\t===Constante===\n");
-    printf("\t\t%i\n", a);
-    printf("\t===Constante===");
-    return 0;
+    printf("\t>> %i\n", a);
+    printf("\t==FimConstante==");
 }
