aa30c5ed877293cd47cc86959ac0fcbdbbcef336
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2021-04-27 19:19:19 -0300

    Create acentuacao.c

---
+++ AlgoritmosC/acentuacao.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+/*
+Autor: Jonatan P. Paschoal
+versao: 1.0.0
+email: jonfisik@hotmail.com
+status: Professor - fisica
+data: 27/04/2021
+*/
+int main()
+{
+	//setlocale(LC_ALL,"Portuguese");
+    setlocale(LC_ALL,"Portuguese");
+    printf("======ACENTUAÇÃO======\n");
+   
+    printf("\ná, â, í, ç, ó, õ...\n");
+    printf("\n======================\n");
+    return 0;
+}
