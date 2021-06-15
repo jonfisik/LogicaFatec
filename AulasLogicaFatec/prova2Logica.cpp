0302e57a959338dbddefe0d447e686ddeadbb9cd
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2021-06-14 22:04:58 -0300

    Create prova2Logica.cpp

---
+++ AulasLogicaFatec/prova2Logica.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Prova lógica
+ * Autor: JPaschoal
+ * versao: 1.0.0
+ * email: jonfisik@hotmail.com
+ * status: Professor - fisica
+ * data: 14/06/2021
+ */
+
+int main(){
+// questão 2	
+/*
+	char ex3[5]={'z','k','w','x','v',};
+	int cont1;
+	cont1 = 2;
+	printf("%c %c", ex3[cont1+1], ex3[4]+cont1);
+*/
+
+// questão 3
+/*
+	int g=0, i=0, l=0, k=0;
+	int m, n, o;
+	while(i<=10){
+		m = g++;
+		n = +l;
+		o = ++k;
+		i++;
+	}
+	
+	printf("m=%d n=%d o=%d", m, n, o);
+*/
+// questão 4
+/*
+	printf("O valor total: %.1f\n", 9.1415169265);
+	return 0;
+*/
+// questão 5
+	int i, j, k;
+	for(i=0, j=1, k=3; i<k; i++){
+		k=k+i%2;
+	}
+	printf("%d, %d, %d", i, j, k);
+// questão 6
+// questão 8
+// questão 9
+// questão 11
+
+}
